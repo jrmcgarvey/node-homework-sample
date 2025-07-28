c411a4c3a14f5455d4a4da06b6059d4808d9f25b
John R. McGarvey <johnmcgarvey55@gmail.com>
2025-07-28 15:11:05 -0400

    renaming of tdd files

--- lesson9TDD/jestConfig/run-lesson9-tdd.js
+++ lesson9TDD/jestConfig/run-lesson9-tdd.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 try {
-  execSync("NODE_ENV=test npx jest --testPathPatterns=test/ --verbose --maxWorkers=1 --config ./lesson11TDD/jestConfig/jest.config.js", { stdio: "inherit" });
+  execSync("NODE_ENV=test npx jest --testPathPatterns=test/ --verbose --maxWorkers=1 --config ./lesson9TDD/jestConfig/jest.config.js", { stdio: "inherit" });
   // console.log("✅ All tests failed as expected (good)");
 } catch (e) {
   // console.log(`name: ${e.name} ${e.message} ${e.stack}`)
